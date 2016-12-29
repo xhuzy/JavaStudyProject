c7f321f4b4904929a5f4468d9f4663f1836a7922
xhuzy <1553746461@qq.com>
2016-12-29 14:39:54 +0800

    jdbcTemplate with transaction and log4J

--- JdbcTemplate-Demo/src/main/java/com/youyou/service/MainTest.java
+++ JdbcTemplate-Demo/src/main/java/com/youyou/service/MainTest.java
@@ -1,15 +1,32 @@
 package com.youyou.service;
 
+import java.io.FileNotFoundException;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.Log4jConfigurer;
 
 public class MainTest {
+
+	/**
+	 * Ìí¼Ó×Ö¶Î×¢ÊÍ.
+	 */
+	protected Logger logger = Logger.getLogger(MainTest.class);
+	static {
+		try {
+			Log4jConfigurer.initLogging("classpath:com/youyou/spring/log4j.properties");
+		} catch (FileNotFoundException ex) {
+			ex.printStackTrace();
+			System.err.println("Cannot Initialize log4j");
+		}
+	}
+
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/youyou/spring/app-context.xml");
 		try {
-
 			ServiceRouteDao dao = (ServiceRouteDao) context.getBean("serviceDao");
-
 			int i = dao.addServiceroute();
 			System.out.println(i);
 		} catch (Exception ex) {
--- JdbcTemplate-Demo/src/main/java/com/youyou/service/ServiceRouteDao.java
+++ JdbcTemplate-Demo/src/main/java/com/youyou/service/ServiceRouteDao.java
@@ -1,20 +1,27 @@
 package com.youyou.service;
 
+import java.util.Date;
+
 import javax.annotation.Resource;
+import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository(value = "serviceDao")
 public class ServiceRouteDao {
 	@Resource(name = "jdbcTemplete")
 	private JdbcTemplate template;
 
-	public int addServiceroute() {
+	/////默认只会对RunTimeException进行rollBack操作，添加上rollbackFor=Exception.class后会对所有异常对进行回滚操作
+	@Transactional(rollbackFor=Exception.class)
+	public int addServiceroute() throws Exception {
 		String sqlStr = "INSERT INTO `zyServiceRouteDB`.`ServiceRoute` (`RouteID`, `ContractName`, `ServiceType`, `CallSystem`, `MachineNO`, `DataCenter`, `UniqueSign`, `BindingType`, `ServiceIP`, `ServicePort`, `SvcPath`, `IsValid`, `ModifyTime`, `IsDelete`, `IsVirtualAddress`, `ProgramName`, `RouteStatus`, `RequestIP`, `ResponseServiceSetID`, `Remark`, `RequestServiceSetID`, `IsValidReason`, `RouteAddType`) VALUES (?, 'com.better517na.hotelGeographyDataService.service.IHotelGeographyDataThriftService', 'java', '酒店基础信息数据服务', '001', 'NW', '888888', 'thrift', '192.168.6.34', '1004', '', '1', '2016-08-05 18:50:08', '0', '0', '', '1', '', '', '', '', '', ?);";
-		return this.template.update(sqlStr, "springjdbcTest", 5);
+		int i = this.template.update(sqlStr, new Date().toString().substring(1, 20), 5);
+		throw new RuntimeException("抛出错误");
 	}
 }
